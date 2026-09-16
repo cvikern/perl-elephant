c4c659aa26993dfac8c6bb39f37c41ffaffe1c20
Christian Vik <99514490+cvikern@users.noreply.github.com>
2026-09-16 20:31:25 +0200

    Add obfuscated Perl script for processing data

---
+++ perl_elephant_claudefable.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+                                    $_=
+                                q{IC=A!g#IC
+                            -A!g*I%CA#g*I!CA*
+                          g*I%C=Ag=O#i?sq#K%i!s :6I,C
+               .A#g?IC*A^g#IC*A  ,g#I?CA!g=I%CA*g*C?i#Ag%I!C?Ag*I:C;
+             A=gL?j%o=6!ID?0#l*JU   !B?A=Q*EB?A!Q#Co#6!O=j?o6*L!i!A#gI
+             *C%A#gI%C?A*g!IA*o=g?I  C%A*g!I#CA*t!Q=EB#A=Q?E%BA#Q=C#U!9L
+            #S#V :AQ=E%B%A*QE#B#A!QE  =B!A#Q?EB=A%J#S%o6*I?C#AK%I=C%A#gI-
+            C#A   %6Q=G!8?q?QE%B!A#Q  E%B=A#Q#CM?u#Q*E?BA=Q?E!BA!Q%E%B=AQ!
+            E!B!AQ!E!B!A%QD*0=g=C?iA,  g*I!CA*g?P*U%Al*J?U#BA%Q*E*B#AQ%E?B
+            ?AL!i!p?A%QE#B%A=Q=EB?A?Q  =EB#A%Q=E!BA%Q#E?BA#O?g?o%gI*C*A#g=I
+           C*p?A#QE=B#A#Q!EB*A!Q?EB#A  ?Q?C!4q%Q#E!BA?Q%E#B*AQ%E=B#A!QE?B#A
+           !QE?B%A=Q*Co*K%I=CA!g=I*C  '5A=Q#E*B#AI*y?N!AQ#E*B%A%QC!s!6=QE!B!
+          A?Q#EB#A=Q!EB!A#Q=E#BA!Q!E  ?B=AK#i%0?lC=i#A#g=Oj=0=j=QE#A?j=O =iA
+          %g?P=S=VA=  K?i=0t#L%k#B`  AJ*T!0?9P?T*0!j*QE*A!r?I0?B=A?Q#D   o#6
+         ^J*Q?og!I     .C*Au!Q%E*   A:lI=C?A^gI*C!A%g*JU?B!A#J=SB?A?Q=   CU?
+    g;I!C%Ag*K!k         ?BA%O.  i#p  %AQ=E!         A#uP!S~  s!K?IC#A   !g:
+        P#UB*A?             O*iA?g=I  ?C#Ag=         I#C#VA?  Q?C=U#gQ   *E:
+        A#l=IC'             A#g#IC%p  *A?Q=D         o!q?Q?E  B%A=I%C#   U9
+       `C#i=Ag              %I=C#N#A  P#S?A!         g#IC?A^  g=IC!A#j  ?Q#
+       EA#l?I               !CV#A?J,  S#Ag?I          *C?A%q  Q?E=A#6P  =U?
+       B%A?Q                C!A!g%IA  *o=g?I          !CB?A?  J#SA*g#I   ;
+       C!Ag~                I!C=A=gK  #k#B%A          I%y?A'  l!QC%M?g
+      `IC?A                 %g?P=UB:  A*O=j*          1A*Q?E  -Ag?I!C*
+      A#KI                  !C%A%uQ   #D!o!g          *IC?A:  g%IC*A%g
+      *I#C                  p=A%Q?C   ;og=J^          U!Aq*I  :C=A%gI-
+      C#t                   ?AQ?D%o   ^9!QE;          B!A*I,  CA=g*C=i
+      A=g                   !I*C!Ag' I%C=Ag*I        ~C!A*gI` C*0%lJ!S
+                          ,U!l%Ky=o!q*K=i4#g=        I*CA%q#K%i?or!J=S*
+                           Ul,J;T'0,g                        I`A;o.K:};
+tr!A-Za-z0-9+/!!cd;tr!A-Za-z0-9+/! -_!;$_=unpack'u',join'',map(chr(32+length
+($_)*3/4).$_,/(.{1,60})/gs);@h=split$/;$_=join$/,map{$r=reverse$h[$_];"$r    "
+.$h[$_]}0..$#h;$t=$_;$t=~s/^/   /gm;print"$t$/$_$/",' 'x25,"just another "
+."perl hacker,$/";
